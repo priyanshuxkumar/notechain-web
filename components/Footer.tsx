305e5e15b33341b53f43fe8eaf210d81c05fd5c9
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-17 11:12:21 +0530

    add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <p className="text-sm align-middle text-center text-muted-foreground py-4">
+        © {new Date().getFullYear()} Dare. All rights reserved.
+      </p>
+    </footer>
+  );
+}
