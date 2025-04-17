eeb2ff7f4a9c0b449ac35e3ba9154e4e6ebf2df2
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-17 13:14:38 +0530

    upadte name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <p className="text-sm align-middle text-center text-muted-foreground py-4">
-        © {new Date().getFullYear()} Dare. All rights reserved.
+        © {new Date().getFullYear()} Notechain. All rights reserved.
       </p>
     </footer>
   );
