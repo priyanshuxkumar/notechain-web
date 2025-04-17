d2cfe254191ee518c72eb12d8e64bf4d07218ceb
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-17 11:15:00 +0530

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
-import Header from "@/components/header/Header";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItsWork";
+import JoinWaitlist from "@/components/JoinWaitlist";
 
 export default function Home() {
   return (
-    <div className="bg-#F2F2F4">
-      <Header />
-
-      <div>hero section</div>
+    <div>
+      <Header/>
+      <main>
+        <Hero/>
+        <div>
+          <HowItWorks/>
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <JoinWaitlist/>
+        </div>
+      </main>
+      <Footer/>
     </div>
   );
 }
